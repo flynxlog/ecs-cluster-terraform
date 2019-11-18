7c607250b19938194c1d44314e533679362d15d3
lynx <imoisilynx@outlook.com>
2019-11-18 13:37:35 -0500

    removed the provider credentials

--- logschedule.tf
+++ logschedule.tf
@@ -1,9 +1,4 @@
-provider "aws" {
-  region     = "us-east-1"
-  version    = "~>2.0"
-  access_key = "AKIASB7ZCA7FAWNTQSFX"
-  secret_key = "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"
-}
+
 
 resource "aws_cloudwatch_event_rule" "every_five_minutes" {
   name                = "every-five-minutes"
